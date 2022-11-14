94a75996ed1d0a4124ff6eddbb412bb80dfb1cd1
josuapinem3unimed@gmail.com <Jospin0>
2022-11-15 06:25:05 +0700

    Diperbaharui oleh josua

--- sources/profile.php
+++ sources/profile.php
@@ -16,17 +16,15 @@
     $query2 = query("SELECT * FROM login WHERE kode_karyawan = '$kode'");
 ?>
 
-<!-- @format -->
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-<meta charset="UTF-8" />
+    <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- Favicon -->
-    <link rel="shortcut icon" href="../../assets/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/img/favicon.ico" type="image/x-icon">
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
@@ -35,6 +33,7 @@
     <!-- Bootstrap Datepicker -->
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="../assets/style/main.css">
     <title>XYZ Company</title>
 </head>
@@ -43,7 +42,6 @@
 
 
 <body class="d-flex">
-
     <!-- Content -->
     <div class="content col d-flex flex-column gap-3 mb-2">
         <!-- Topbar -->
@@ -71,7 +69,6 @@
                     <label class="btn text material-icons-round p-1" for="dark-mode">&#xef5e</label>
                 </div>
 
-                
                 <!-- Dropdown -->
                 <div class="dropdown d-flex">
                     <!-- Toggle Dropdown -->
@@ -90,8 +87,8 @@
                     <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end">
                         <li>
                             <a class="dropdown-item d-flex d-lg-none flex-column">
-                                <span class="fs-5"><?= strtoupper($query['nama']); ?></span>
-                                <span class="fs-5 fw-bold text-danger"><?= strtoupper($query['posisi']); ?></span>
+                                <span class="fs-5">Roberto Firmino</span>
+                                <span class="fs-5 fw-bold text-danger">CEO</span>
                             </a>
                         </li>
                         <li>
@@ -107,7 +104,7 @@
                             <hr class="dropdown-divider d-block d-lg-none">
                         </li>
                         <li>
-                            <a class="dropdown-item d-block d-lg-none" href="../../php/logoutController.php"
+                            <a class="dropdown-item d-block d-lg-none" href="../php/logoutController.php"
                                 onclick="return confirm('Apakah anda ingin logout')">Sign Out</a>
                         </li>
                     </ul>
@@ -120,7 +117,6 @@
             <!-- Content Header -->
             <div class="d-flex my-2 mx-0 mx-lg-4 px-1 gap-1">
                 <div class="p-2 d-flex align-items-center">
-                    
                     <div class="d-flex">
                         <a type="button" href="../php/profileController.php"
                             class="d-flex rounded-circle p-1 btn btn-primary align-items-center justify-content-center">
@@ -132,14 +128,14 @@
                     <span class="fs-3 fw-bold">Profile</span>
                     <ol class="d-flex my-0 breadcrumb p-lg-2">
                         <li class="breadcrumb-item"><a class="text-decoration-none"
-                                href="admin/admin-dashboard.html">Home</a>
+                                href="./admin/admin-dashboard.php">Home</a>
                         </li>
                         <li class="breadcrumb-item active" aria-current="page">Profile</li>
                     </ol>
                 </div>
             </div>
-            <form action="" method="POST" enctype="multipart/form-data">
-                <!-- Content Body -->
+
+            <!-- Content Body -->
             <div class="d-flex mx-1 mx-lg-4 mb-4 p-2 justify-content-center">
                 <!-- Profile -->
                 <div class="flex-container d-flex flex-column gap-3 p-4 rounded-4 ">
@@ -152,24 +148,23 @@
                     <div class="d-flex justify-content-center p-3">
                         <div class="d-flex position-relative">
                             <img id="profile-img-container"
-                                src="../sources/user-img/<?php echo $query['foto'] ?>"
+                                src="https://cdn.discordapp.com/attachments/1020601540257521674/1037712201202552882/person_filled_FILL0_wght400_GRAD0_opsz48.png"
                                 class="rounded-circle bg-light col-2 border " alt="Avatar" />
-                            <a href="#" id="a"
-                                class="btn btn-primary rounded-circle position-absolute d-flex p-2 change-image">
+                            <a class="btn btn-primary rounded-circle position-absolute d-flex p-2 change-image">
                                 <i class="material-icons-round fs-6">&#xe439</i>
-                                <input type="file" name="foto" class="file-input" id="file" hidden>
                             </a>
                         </div>
 
                     </div>
 
                     <!-- Profile Details -->
+                <form action="" method="POST" enctype="multipart/form-data">
                     <div class="d-flex flex-column gap-3 mt-2">
                         <div class="d-flex">
                             <h4 class="fw-semibold header">
                                 Personal Information
                             </h4>
-                            <a href="#" class="btn btn-primary ms-auto d-flex p-2" id="edit-profile">
+                            <a class="btn btn-primary ms-auto d-flex p-2" id="edit-profile">
                                 <i class="material-icons-round">&#xe3c9</i>
                             </a>
                         </div>
@@ -201,12 +196,12 @@
                                 <div class="d-flex px-3 py-2 gap-4 background rounded-4">
                                     <div class="d-flex gap-4">
                                         <div class="d-flex align-items-center">
-                                            <i class="material-icons-round fs-1">&#xe0cd</i>
+                                            <i class="material-icons-round fs-1">&#xf045</i>
                                         </div>
                                         <div class="d-flex flex-column">
-                                            <span class="fw-semibold">Phone</span>
-                                            <input class="user-edit form-control text text-field profile-info"
-                                                type="text" value="081234567890" aria-label="readonly input example"
+                                            <span class="fw-semibold">NIP</span>
+                                            <input class="user-edit form-control text text-field profile-info" name="nip"
+                                                type="text" value="<?= $query['nip']; ?>" aria-label="readonly input example"
                                                 readonly>
                                         </div>
                                     </div>
@@ -219,8 +214,9 @@
                                     </div>
                                     <div class="d-flex flex-column">
                                         <span class="fw-semibold">Birth Date</span>
-                                        <input class="user-edit form-control text text-field profile-info" type="date" name="tanggal" id="datepicker"
-                                            value="<?= $query['tanggal']; ?>" aria-label="readonly input example" readonly>
+                                        <input class="user-edit form-control text text-field profile-info" name="tanggal"
+                                            id="datepicker" type="text" value="<?= $query['tanggal']; ?>"
+                                            aria-label="readonly input example" readonly>
                                     </div>
                                 </div>
                                 <div class="d-flex px-3 py-2 gap-4 background rounded-4">
@@ -230,8 +226,12 @@
                                         </div>
                                         <div class="d-flex flex-column">
                                             <span class="fw-semibold">Gender</span>
-                                            <input class="user-edit form-control text text-field profile-info user-edit" name="gender"
-                                                type="text" value="<?= $query['jenis_kelamin']; ?>" aria-label="readonly input example" readonly>
+                                            <select
+                                                class="form-select user-edit form-control text text-field profile-info" name="gender"
+                                                aria-label="Default select example" value="<?= $query['jenis_kelamin']; ?>" disabled>
+                                                <option value="Male" name="gender">Male</option>
+                                                <option value="Female" name="gender">Female</option>
+                                            </select>
                                         </div>
                                     </div>
                                 </div>
@@ -250,12 +250,12 @@
                                 </div>
                             </div>
                         </div>
-                        <button type="submit" href="" class="btn btn-primary my-2 me-auto p-2 gap-2 d-none" id="save-profile" name="simpan">
+                        <button type="submit" name="simpan" href="#" class="btn btn-primary my-2 me-auto p-2 gap-2 d-none" id="save-profile">
                             <i class="material-icons-round">&#xe161</i>
                             <span>Simpan</span>
                         </button>
                     </div>
-            </form>
+                </form>    
                     <div class="d-flex flex-column gap-3 mt-2">
                         <div class="d-flex">
                             <h4 class="fw-semibold header">
@@ -270,8 +270,16 @@
                                     </div>
                                     <div class="d-flex flex-column">
                                         <span class="fw-semibold">Position</span>
-                                        <input class="form-control text text-field profile-info" type="text" value="<?= strtoupper($query['posisi']); ?>"
-                                            aria-label="readonly input example" readonly>
+                                        <select class="form-select text text-field profile-info"
+                                            aria-label="Default select example" disabled>
+                                            <option value="admin">Admin</option>
+                                            <option value="android-developer">Android Developer</option>
+                                            <option value="ceo">CEO</option>
+                                            <option value="data-analyst">Data Analyst</option>
+                                            <option value="hrd">HRD</option>
+                                            <option value="it-security">IT Security</option>
+                                            <option value="web-developer">Web Developer</option>
+                                        </select>
                                     </div>
                                 </div>
                             </div>
@@ -283,7 +291,7 @@
                                     <div class="d-flex flex-column">
                                         <span class="fw-semibold">Salary</span>
                                         <input class="form-control text text-field profile-info" type="text"
-                                            value="Rp. <?= $query['gaji']; ?>" aria-label="readonly input example" readonly>
+                                            value="Rp. 10,000,000" aria-label="readonly input example" readonly>
                                     </div>
                                 </div>
                             </div>
@@ -298,23 +306,26 @@
                 <!-- JQuery Library -->
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
+                <!-- Bootstrap Datepicker -->
+                <script
+                    src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+
+                <!-- Custom JS -->
                 <script src="../assets/script/main.js"></script>
-
-                <script src="../assets/script/editFoto.js"></script>
 </body>
-
-</html>
 <?php
 
     if(isset($_POST['simpan'])){
         $nama = $_POST['nama'];
-        $tanggal = $_POST['tanggal'];
+        $tanggal = strtotime($_POST['tanggal']);
+        $newdate = date('Y-m-d',$tanggal);
         $email = $_POST['email'];
         $gender = $_POST['gender'];
         $alamat = $_POST['alamat'];
+        $nip = $_POST['nip'];
 
         $id = $_SESSION['kode'];
-        $update = mysqli_query($koneksi, "UPDATE karyawan_tetap SET nama='$nama', tanggal='$tanggal', jenis_kelamin='$gender', alamat='$alamat' WHERE kode_karyawan='$id'");
+        $update = mysqli_query($koneksi, "UPDATE karyawan_tetap SET nama='$nama', nip='$nip' ,tanggal='$newdate', jenis_kelamin='$gender', alamat='$alamat' WHERE kode_karyawan='$id'");
         $update .= mysqli_query($koneksi, "UPDATE login SET email='$email' WHERE kode_karyawan='$id'");
 
         if($update){
@@ -322,3 +333,4 @@
         }
     }
 ?>
+</html>
